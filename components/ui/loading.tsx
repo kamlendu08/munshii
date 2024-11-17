a7e03bd3b6e520c6497cb109c6847132dbda2725
kamlendu08 <kamlendubala@gmail.com>
2024-11-18 00:56:04 +0530

    correcting

--- components/ui/loading.tsx
+++ components/ui/loading.tsx
@@ -3,7 +3,7 @@ import { Skeleton } from './skeleton'
 
 export default function Loading() {
     return (
-        <div className="flex justify-center  min-h-screen bg-white px-5 mt-6 sm:px-20">
+        <div className="flex justify-center  min-h-screen bg-white mx-5 mt-6 sm:px-20">
             <div className="flex flex-col items-center space-y-3">
                 <Skeleton className="h-[125px] w-[500px] rounded-xl" />
                 <div className="space-y-2">
